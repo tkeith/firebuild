6a4a85b2722f2d922385091c13b27e91d03e544d
Trevor Keith <tk@tkeith.com>
2023-09-10 09:21:37 -0500

    add user request logic

---
+++ src/lib/util.ts
@@ -0,0 +1,163 @@
+import { exec, execFile } from "child_process";
+import { promisify } from "util";
+import * as path from "path";
+import * as fsPromises from "fs/promises";
+import { z } from "zod";
+import readline from "readline";
+
+export const execPromise = promisify(exec);
+
+export const execFilePromise = promisify(execFile);
+
+export function safePathJoin(base: string, target: string) {
+  // Resolves to an absolute path
+  const targetPath = path.join(base, target);
+
+  // Checks if resolved path is still within the base directory
+  if (!targetPath.startsWith(base)) {
+    throw new Error("Upward traversal outside of `base` not allowed");
+  }
+
+  return targetPath;
+}
+
+export async function pathAccessible(path: string) {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export async function pathIsFile(path: string) {
+  try {
+    const stat = await fsPromises.stat(path);
+    const isFile = stat.isFile();
+    return isFile;
+  } catch (e) {
+    return false;
+  }
+}
+
+export async function pathIsDir(path: string) {
+  try {
+    const stat = await fsPromises.stat(path);
+    return stat.isDirectory();
+  } catch {
+    return false;
+  }
+}
+
+// emulate the mkdir -p behavior
+export async function mkdirP(path: string) {
+  const pathParts = path.split("/");
+  for (let i = 0; i < pathParts.length; i++) {
+    const partialPath = pathParts.slice(0, i + 1).join("/");
+    if (partialPath === "") {
+      continue;
+    }
+    if (!(await pathIsDir(partialPath))) {
+      if (await pathAccessible(partialPath)) {
+        throw Error(
+          `path ${partialPath} already exists and is not a directory`
+        );
+      }
+      await fsPromises.mkdir(partialPath);
+    }
+  }
+}
+
+export const parsedJsonSchema: z.ZodSchema<ParsedJson> = z.lazy(() =>
+  z.union([
+    z.string(),
+    z.number(),
+    z.boolean(),
+    z.null(),
+    z.array(parsedJsonSchema),
+    z.record(parsedJsonSchema),
+  ])
+);
+
+export type ParsedJson =
+  | boolean
+  | number
+  | string
+  | null
+  | ParsedJson[]
+  | { [key: string]: ParsedJson };
+
+function preprocessGptJson(jsonString: string) {
+  let insideString = false;
+  let result = "";
+  let wasBackslash = false; // track if the previous character was a backslash
+
+  for (let i = 0; i < jsonString.length; i++) {
+    const char = jsonString[i];
+
+    if (char === '"' && !wasBackslash) {
+      insideString = !insideString;
+    }
+
+    if (insideString && (char === "\n" || char === "\r")) {
+      result += "\\n";
+    } else {
+      result += char;
+    }
+
+    wasBackslash = char === "\\";
+  }
+
+  return result;
+}
+
+export function parseGptJson(jsonString: string) {
+  const preprocessedString = preprocessGptJson(jsonString);
+  return parsedJsonSchema.parse(JSON.parse(preprocessedString));
+}
+
+export async function askUserMultiLine(question: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let request: string[] = [];
+
+  return new Promise((resolve, reject) => {
+    rl.on("line", (input: string) => {
+      if (input === ".") {
+        rl.close();
+        resolve(request.join("\n"));
+      } else {
+        request.push(input);
+      }
+    });
+
+    rl.on("error", (err: Error) => {
+      reject(err);
+    });
+
+    console.log(`${question} (end with a single dot on a line):`);
+  });
+}
+
+export async function askUserSingleLine(question: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve, reject) => {
+    rl.on("line", (input: string) => {
+      rl.close();
+      resolve(input);
+    });
+
+    rl.on("error", (err: Error) => {
+      reject(err);
+    });
+
+    console.log(`${question} (respond and hit enter):`);
+  });
+}
--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -1,16 +1,249 @@
-import { callGptApi } from "@/lib/gptApi";
+import { Message, callGptApi } from "@/lib/gptApi";
+import {
+  askUserMultiLine,
+  askUserSingleLine,
+  execPromise,
+  mkdirP,
+  parseGptJson,
+  pathIsFile,
+  safePathJoin,
+} from "@/lib/util";
+import { z } from "zod";
+import * as fsPromises from "fs/promises";
+import path from "path";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+const SYSTEM_PROMPT = `\
+# Overview
+
+You are a programming AI designed to automatically help the user (programmer) to work with Fireblocks, a platform for managing cryptocurrency wallets. You will be using the Fireblocks NCW (non-custodial wallet) API. You can also help the user set up their local environment, for example by generating the certificate needed to interact with the Fireblocks API. When you are done helping a user, call the \`complete_request\` function.
+
+# Here are some reference materials that may help
+
+## Generate CSR file
+
+  openssl req -new -newkey rsa:4096 -nodes -keyout fireblocks_secret.key -out fireblocks.csr -subj "/CN=My Fireblocks Certificate"
+
+`;
+
+type Context = {
+  codePath: string;
+};
+
+const GPT_FUNCTIONS = [
+  {
+    name: "list_files",
+    description: "List all files, recursively, in the project",
+    parametersSchema: z.object({}),
+    execute: async (context: Context, args: {}) => {
+      console.log("Listing files...");
+      const result = (
+        await execPromise(`cd ${context.codePath} && tree --gitignore .`)
+      ).stdout.trim();
+      return result;
+    },
+  },
+
+  {
+    name: "read_file",
+    description: "Read a file",
+    parametersSchema: z.object({
+      path: z.string().describe("Path relative to the project root"),
+    }),
+    execute: async (context: Context, args: { path: string }) => {
+      console.log(`Reading file ${args.path}...`);
+      const filePath = safePathJoin(context.codePath, args.path);
+      if (!(await pathIsFile(filePath))) {
+        return "<file does not exist>";
+      } else {
+        return await fsPromises.readFile(filePath, "utf-8");
+      }
+    },
+  },
+
+  {
+    name: "write_file",
+    description:
+      "Write or overwrite file. Delete if content empty. Directories will be automatically created as needed.",
+    parametersSchema: z.object({
+      path: z.string().describe("Path relative to project."),
+      content: z.string().describe("Content to write."),
+    }),
+    execute: async (
+      context: Context,
+      args: { path: string; content: string }
+    ) => {
+      console.log(`Writing file ${args.path}...`);
+
+      const filePath = safePathJoin(context.codePath, args.path);
+      const dirPath = path.dirname(filePath);
+
+      // if content is empty, delete the file if it exists
+      if (!args.content.trim()) {
+        if (await pathIsFile(filePath)) {
+          await fsPromises.rm(filePath);
+        }
+      } else {
+        await mkdirP(dirPath);
+        await fsPromises.writeFile(filePath, args.content);
+      }
+      return "<done>";
+    },
+  },
+
+  {
+    name: "run",
+    description: "Run command in bash.",
+    parametersSchema: z.object({
+      command: z.string().describe(`Bash command to run.`),
+    }),
+    execute: async (context: Context, args: { command: string }) => {
+      console.log(`GPT wants to run command:\n  ${args.command}`);
+      const userResponse = await askUserSingleLine(
+        "Do you want to run this command? (y/n)"
+      );
+
+      if (userResponse.toLowerCase() !== "y") {
+        return "<user canceled request>";
+      }
+
+      let execRes;
+      let resPrefix = "";
+      try {
+        execRes = await execPromise(args.command, {
+          cwd: context.codePath,
+        });
+        resPrefix = `<done>\n\n`;
+      } catch (e) {
+        try {
+          const errorDetails = z
+            .object({
+              code: z.number(),
+              stdout: z.string(),
+              stderr: z.string(),
+            })
+            .parse(e);
+          resPrefix = `<failed with code ${errorDetails.code}>\n\n`;
+          execRes = errorDetails;
+        } catch (parseError) {
+          throw e;
+        }
+      }
+      return (
+        `${resPrefix}${execRes.stdout.trim()}` +
+        (execRes.stderr.trim() ? `\n\nstderr:\n\n${execRes.stderr.trim()}` : "")
+      ).trim();
+    },
+  },
+
+  {
+    name: "complete_request",
+    description: "Complete the request.",
+    parametersSchema: z.object({
+      summary: z.string().describe("Summary to the user of what was done"),
+    }),
+    execute: async (context: Context, args: { summary: string }) => {
+      console.log(args.summary);
+      console.log("\n=== Request complete ===\n");
+      return "<end_conversation>";
+    },
+  },
+];
+
+const FUNCTION_DEFINITIONS_FOR_GPT = GPT_FUNCTIONS.map((fn) => ({
+  name: fn.name,
+  description: fn.description,
+  parameters: zodToJsonSchema(fn.parametersSchema),
+}));
+
+async function handleFunctionCall(
+  context: Context,
+  functionCall: {
+    name: string;
+    arguments: string;
+  }
+): Promise<string> {
+  const fn = GPT_FUNCTIONS.find((fn) => fn.name === functionCall.name);
+  if (!fn) {
+    throw new Error(`Function ${functionCall.name} not found.`);
+  }
+
+  let args: any;
+
+  try {
+    const rawParsedJson = parseGptJson(functionCall.arguments);
+    args = fn.parametersSchema.parse(rawParsedJson);
+  } catch (e) {
+    return `<error parsing arguments>`;
+  }
+
+  const res = await fn.execute(context, args);
+
+  return res;
+}
+
+async function handleUserRequest(context: Context, request: string) {
+  let messages: Message[] = [
+    { role: "system", content: SYSTEM_PROMPT },
+    { role: "user", content: request },
+    {
+      role: "assistant",
+      content: "First, let's list the files in the project.",
+      function_call: { name: "list_files", arguments: "{}" },
+    },
+  ];
+
+  while (true) {
+    const lastMessage = messages[messages.length - 1]!;
+    if (lastMessage.role === "assistant" && lastMessage.function_call) {
+      const functionRes: string = await handleFunctionCall(
+        context,
+        lastMessage.function_call
+      );
+      if (functionRes == "<end_conversation>") {
+        return;
+      }
+      messages.push({
+        role: "function",
+        name: lastMessage.function_call.name,
+        content: functionRes,
+      });
+      continue;
+    }
+    if (
+      lastMessage.role === "user" ||
+      lastMessage.role === "function" ||
+      lastMessage.role === "assistant"
+    ) {
+      const gptRes = await callGptApi(
+        messages,
+        FUNCTION_DEFINITIONS_FOR_GPT,
+        "gpt-4"
+      );
+      if (!gptRes.success) {
+        console.log("gpt error:", gptRes.error);
+        throw new Error("gpt failed");
+      }
+      const newMessage = gptRes.newMessage;
+      messages.push(newMessage);
+      console.log("GPT:");
+      console.log(newMessage.content);
+      console.log();
+      continue;
+    }
+    throw new Error("unreachable");
+  }
+}
 
 async function main() {
-  const res = await callGptApi(
-    [
-      { role: "system", content: "you are a poem writer" },
-      { role: "user", content: "write me a poem about bitcoin" },
-    ],
-    [],
-    "gpt-4"
+  const codePath = await askUserSingleLine(
+    "What is the path to your code directory?"
   );
-  if (res.success) {
-    console.log(res.newMessage);
+
+  while (true) {
+    console.log("\n");
+    const request = await askUserMultiLine("What do you want to do?");
+    await handleUserRequest({ codePath }, request);
   }
 }
 
